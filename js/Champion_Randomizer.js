4446b3da120574259d4fdb85bda0179b07968765
RedHulk05 <dme_dinis@hotmail.com>
2022-07-03 01:57:03 +0100

    primeiro commit

---
+++ js/Champion_Randomizer.js
@@ -0,0 +1,154 @@
+let MidChampList = ['Aatrox', 'Ahri', 'Akali', 'Akshan', 'Annie', 'Aurelion Sol', 'Azir', 'Anivia', 'Cassiopeia', 'Corki', 'Diana', 'Ekko', 'Fizz', 'Galio', 'Jayce', 'Kassadin', 'Katarina', 'Leblanc', 'Lissandra', 'Lucian', 'Lux', 'Malzahar', 'Nekko', 'Orianna', 'Pantheon', 'Pyke', 'Qiyana', 'Renekton', 'Rumble', 'Ryze', 'Swain', 'Sylas', 'Syndra', 'Talliyah', 'Talon', 'Twisted Fate', 'Veigar', 'Velkoz', 'Viktor', 'Vladimir', 'Xerath', 'Yasuo', 'Yone', 'Zed', 'Ziggs', 'Zilean', 'Zoe', 'Gangplank', 'Heimer', 'Zyra'];
+let TopChampList = ['Aatrox', 'Akali', 'Camile', 'Cassiopeia', 'Chogath', 'Darius', 'Dr. Mundo', 'Fiora', 'Gangplank', 'Garen', 'Gnar', 'Gwen', 'Heimerdinger', 'illaoi', 'Irelia', 'Jax', 'Jayce', 'Karma', 'Kayle', 'Kennen', 'Kled', 'Malphite', 'Maokai', 'Mordekaiser', 'Nasus', 'Olaf', 'Ornn', 'Pantheon', 'Poppy', 'Quinn', 'Renekton', 'Rengar', 'Riven', 'Rumble', 'Ryze', 'Sett', 'Shen', 'Singed', 'Sion', 'Sylas', 'Tahm Kench', 'Teemo', 'Tryndamere', 'Urgot', 'Vayne', 'Vladimir', 'Volibear', 'Wukong', 'Warwick', 'Yasuo', 'Yone', 'Yorick', 'Zac'];
+let JungleChampList = ['Amumu', 'Belveth', 'Chogath', 'Diana', 'Dr. Mundo', 'Ekko', 'Elise', 'Evelyn', 'Fiddlesticks', 'Gragas', 'Graves', 'Hecarin', 'Ivern', 'Jarvan', 'Jax', 'Kartus', 'Kayn', 'Khazix', 'Kindred', 'LeeSin', 'Lillia', 'Master Yi', 'Mordekaiser', 'Nidalee', 'Nocturne', 'Nunu', 'Olaf', 'Poppy', 'Rammus', 'Reksai', 'Rengar', 'Sejuani', 'Sett', 'Shaco', 'Shyvana', 'Skarner', 'Talliyah', 'Trundle', 'Udyr', 'Vi', 'Viego', 'Volibear', 'Warwick', 'Wukong', 'XinZao', 'Zac'];
+let BotChampList = ['Aphelios', 'Ashe', 'Caitlyn', 'Draven', 'Ezreal', 'Graves', 'Jhin', 'Jinx', 'Kaisa', 'Kalista', 'Kindred', 'Kogmaw', 'Lucian', 'Miss Fortune', 'Samira', 'Senna', 'Quinn', 'Sivir', 'Tristana', 'Twitch', 'Varus', 'Vayne', 'Xayah', 'Yasuo', 'Zeri', 'Akshan']
+let SuppChampList = ['Alistar', 'Amumu', 'Bard', 'Blitzcrank', 'Brand', 'Braum', 'Galio', 'Janna', 'Karma', 'Leona', 'Lulu', 'Lux', 'Malphite', 'Maokai', 'Morgana', 'Nami', 'Nautilus', 'Pantheon', 'Poppy', 'Pyke', 'Rakan', 'Renata', 'Rell', 'Senna', 'Seraphine', 'Sett', 'Shaco', 'Shen', 'Sona', 'Soraka', 'Swain', 'Tahm Kench', 'Taric', 'Thresh', 'Velkoz', 'Vex', 'Xerath', 'Yuumi', 'Zac', 'Zilean', 'Zyra'];
+let AllChampList = ['Aatrox', 'Ahri', 'Akali', 'Akshan', 'Alistar', 'Amumu', 'Anivia', 'Annie', 'Aphelios', 'Ashe', 'Aurelion Sol', 'Azir', 'Bard', 'Belveth', 'Blitzcrank', 'Brand', 'Braum', 'Caitlyn', 'Camile', 'Cassiopeia', 'Chogath', 'Corki', 'Darius', 'Diana', 'Draven', 'Dr. Mundo', 'Ekko', 'Elise', 'Evelyn', 'Ezreal', 'Fiddlesticks', 'Fiora', 'Fizz', 'Galio', 'Gangplank', 'Garen', 'Gnar', 'Gragas', 'Graves', 'Gwen', 'Hecarin', 'Heimerdinger', 'illaoi', 'Irelia', 'Ivern', 'Janna', 'Jarvan', 'Jax', 'Jayce', 'Jhin', 'Jinx', 'Kaisa', 'Kalista', 'Karma', 'Kartus', 'Kassadin', 'Katarina', 'Kayle', 'Kayn', 'Kennen', 'Khazix', 'Kindred', 'Kled', 'Kogmaw', 'Leblanc', 'LeeSin', 'Leona', 'Lillia', 'Lissandra', 'Lucian', 'Lulu', 'Lux', 'Malphite', 'Malzahar', 'Master Yi', 'Miss Fortune', 'Maokai', 'Mordekaiser', 'Morgana', 'Nami', 'Nasus', 'Nautilus', 'Nekko', 'Nidalee', 'Nocturne', 'Nunu', 'Olaf', 'Orianna', 'Ornn', 'Pantheon', 'Poppy', 'Pyke', 'Qiyana', 'Quinn', 'Rakan', 'Rammus', 'Reksai', 'Renata', 'Rell', 'Renekton', 'Rengar', 'Riven', 'Rumble', 'Ryze', 'Samira', 'Sejuani', 'Senna', 'Seraphine', 'Sett', 'Shaco', 'Shen', 'Shyvana', 'Singed', 'Sion', 'Sivir', 'Skarner', 'Sona', 'Soraka', 'Swain', 'Sylas', 'Syndra', 'Tahm Kench', 'Talliyah', 'Talon', 'Taric', 'Teemo', 'Twisted Fate', 'Thresh', 'Tristana', 'Trundle', 'Tryndamere', 'Twitch', 'Udyr', 'Urgot', 'Varus', 'Vayne', 'Veigar', 'Velkoz', 'Vex', 'Vi', 'Viego', 'Viktor', 'Vladimir',  'Volibear', 'Wukong', 'Warwick', 'Xayah', 'Xerath', 'XinZao', 'Yasuo', 'Yone', 'Yorick', 'Yuumi', 'Zac', 'Zed', 'Zeri', 'Ziggs', 'Zilean', 'Zoe', 'Zyra'];
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+let starBtn = document.getElementById('start');
+let acptBtn = document.getElementById('accept');
+let rfsBtn = document.getElementById('refuse');
+var role = 0
+
+
+function Mid() {
+  role = 1;
+  SelectedBtn('mid');
+}
+function Top() {
+  role = 2;
+  SelectedBtn('top');
+}
+function All() {
+  role = 6;
+  SelectedBtn('all');
+}
+function Adc() {
+  role = 4;
+  SelectedBtn('adc');
+}
+function Supp() {
+  role = 5;
+  SelectedBtn('supp');
+}
+function Jgl() {
+  role = 3;
+  SelectedBtn('jgl');
+}
+function MidRandom() {
+    clear();
+    const randomElement = MidChampList[Math.floor(Math.random() * MidChampList.length)];
+    var img =  randomElement + '.png';
+    champ = new Image();
+    champ.src = './img/champions/' + img;
+    draw();
+    document.getElementById('ChampName').textContent = randomElement;
+}
+function TopRandom() {
+  clear();
+  const randomElement = TopChampList[Math.floor(Math.random() * TopChampList.length)];
+  var img =  randomElement + '.png';
+  champ = new Image();
+  champ.src = './img/champions/' + img;
+  draw();
+  document.getElementById('ChampName').textContent = randomElement;
+}
+function JungleRandom() {
+  clear();
+  const randomElement = JungleChampList[Math.floor(Math.random() * JungleChampList.length)];
+  var img =  randomElement + '.png';
+  champ = new Image();
+  champ.src = './img/champions/' + img;
+  draw();
+  document.getElementById('ChampName').textContent = randomElement;
+}
+function BotRandom() {
+  clear();
+  const randomElement = BotChampList[Math.floor(Math.random() * BotChampList.length)];
+  var img =  randomElement + '.png';
+  champ = new Image();
+  champ.src = './img/champions/' + img;
+  draw();
+  document.getElementById('ChampName').textContent = randomElement;
+}
+function SuppRandom() {
+  clear();
+  const randomElement = SuppChampList[Math.floor(Math.random() * SuppChampList.length)];
+  var img =  randomElement + '.png';
+  champ = new Image();
+  champ.src = './img/champions/' + img;
+  draw();
+  document.getElementById('ChampName').textContent = randomElement;
+}
+function AllRandom() {
+  clear();
+  const randomElement = AllChampList[Math.floor(Math.random() * AllChampList.length)];
+  var img =  randomElement + '.png';
+  champ = new Image();
+  champ.src = './img/champions/' + img;
+  draw();
+  document.getElementById('ChampName').textContent = randomElement;
+}
+function Random() {
+  if (role == 1) {
+    MidRandom();
+    site = 'https://u.gg/lol/champions/' + document.getElementById('ChampName').textContent + '/build';
+    mudarBtn();
+  } else if (role == 2) {
+    TopRandom();
+    site = 'https://u.gg/lol/champions/' + document.getElementById('ChampName').textContent + '/build';
+    mudarBtn();
+    site = 'https://u.gg/lol/champions/' + document.getElementById('ChampName').textContent + '/build';
+  } else if (role == 3) {
+    JungleRandom();
+    site = 'https://u.gg/lol/champions/' + document.getElementById('ChampName').textContent + '/build';
+    mudarBtn();
+  } else if (role == 4) {
+    BotRandom();
+    site = 'https://u.gg/lol/champions/' + document.getElementById('ChampName').textContent + '/build';
+    mudarBtn();
+  } else if (role == 5) {
+    SuppRandom();
+    site = 'https://u.gg/lol/champions/' + document.getElementById('ChampName').textContent + '/build';
+    mudarBtn();
+  } else if (role == 6) {
+    AllRandom();
+    site = 'https://u.gg/lol/champions/' + document.getElementById('ChampName').textContent + '/build';
+    mudarBtn();
+  } else if (role == 0) {
+    alert("Escolha uma lane, por favor!");
+  }
+}
+function draw() {
+  ctx.globalCompositeOperation = 'destination-over';
+  champ.onload = () =>{
+    ctx.drawImage(champ, 0, 0);
+  }
+}
+function clear() {
+  ctx.clearRect(0, 0, canvas. width, canvas. height);
+}
+function SelectedBtn(id) {
+  document.getElementById('mid').style.boxShadow = '';
+  document.getElementById('top').style.boxShadow = '';
+  document.getElementById('jgl').style.boxShadow = '';
+  document.getElementById('adc').style.boxShadow = '';
+  document.getElementById('supp').style.boxShadow = '';
+  document.getElementById('all').style.boxShadow = '';
+  document.getElementById(id).style.boxShadow = 'rgba(255, 255, 255, 0.837) 0px -50px 36px -28px inset';
+}
+function mudarBtn() {
+  starBtn.style.display='none';
+  acptBtn.style.display='inline';
+  rfsBtn.style.display='inline';
+}
+function voltarBtn() {
+  starBtn.style.display='block';
+  acptBtn.style.display='none';
+  rfsBtn.style.display='none';
+}
+function abrirSite() {
+  window.open(site);
+}
+//https://getcssscan.com nice site to css things
